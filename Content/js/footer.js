7e2a79870a0b22867ccf74518b670f489ca25a84
Ihor Fedorchuk <fihor@mail.ua>
2016-01-30 18:04:09 +0200

    new year 2016 and christmas 2016

---
+++ Content/js/footer.js
@@ -0,0 +1,20 @@
+﻿document.write('\
+<br/ >\
+        <br />\
+        <footer id="content">\
+            <div class="row">\
+                <div class="col-md-8 col-md-offset-2">\
+                    <hr />\
+                    <p class="text-right">&copy; <span id="yyyy"></span> - ФІА</p>\
+                </div>\
+            </div>\
+        </footer>\
+    </div>\
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>\
+<script src="Content/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>\
+</body>\
+</html>\
+\
+');
+var year = document.getElementById("yyyy");
+year.innerHTML = new Date().getFullYear().toString();
